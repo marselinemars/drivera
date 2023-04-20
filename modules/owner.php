d540721a54f1109e36e129d17d98c2c0b47f36a0
djamo <djamila.hamza@ensia.edu.dz>
2023-04-20 07:46:26 +0100

    remake of the html/css page of the register to fit the implementation

--- modules/owner.php
+++ modules/owner.php
@@ -14,6 +14,25 @@ switch($vars['action']){
     }break;
 
 
+    case "register":{
+        
+        include("view/register.php");
+
+        exit;
+    }break;
+    
+    case "do_register":{
+
+         $ret=user_process_signup($vars);
+         
+         if ($ret['status']==1){
+            header("location: index.php?action=dashboard"); 
+         }else{
+            header("location: index.php?action=register&error_message=".urlencode($ret['error']));
+         }
+         exit;
+    }break;
+
     
 }
 
